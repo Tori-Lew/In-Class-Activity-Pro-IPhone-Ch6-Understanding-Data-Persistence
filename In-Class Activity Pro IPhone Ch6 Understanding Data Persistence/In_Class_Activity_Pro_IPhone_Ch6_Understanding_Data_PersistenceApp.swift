1da4b7fa75dc8326be0961bba0dd140e2b4fc77e
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-06 16:56:13 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch6 Understanding Data Persistence/In_Class_Activity_Pro_IPhone_Ch6_Understanding_Data_PersistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch6_Understanding_Data_PersistenceApp.swift
+//  In-Class Activity Pro IPhone Ch6 Understanding Data Persistence
+//
+//  Created by Student Account on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch6_Understanding_Data_PersistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
