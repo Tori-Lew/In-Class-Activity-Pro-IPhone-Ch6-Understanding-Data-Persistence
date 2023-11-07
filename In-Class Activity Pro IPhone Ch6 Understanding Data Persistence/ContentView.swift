a3e4b2a3a1c1f2a97205a35c390b18b119c76346
Tori Lew <torirlew@gmail.com>
2023-11-06 18:20:18 -0800

    Dark mode toggle data is persistant

--- In-Class Activity Pro IPhone Ch6 Understanding Data Persistence/ContentView.swift
+++ In-Class Activity Pro IPhone Ch6 Understanding Data Persistence/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var myToggle = false
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(.accent)
             Text("Hello, world!")
+            Toggle(isOn: $myToggle, label: {
+                Text("Dark Mode")
+            })
         }
         .padding()
+        .onChange(of: myToggle) {
+            UserDefaults.standard.set(myToggle, forKey: "darkModeEnabled")
+            myToggle = UserDefaults.standard.bool(forKey: "darkModeEnabled")
+        }
+        .preferredColorScheme(UserDefaults.standard.bool(forKey: "darkModeEnabled") ? .light : .dark)
     }
 }
 
