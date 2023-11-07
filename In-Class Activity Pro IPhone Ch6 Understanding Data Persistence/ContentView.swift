4d6fd6108e61a508f7c6b1ca5bf9331710c6f072
Tori Lew <torirlew@gmail.com>
2023-11-06 21:09:06 -0800

    Moved the core data in content view so that it is within the vstack. fixed toggle

--- In-Class Activity Pro IPhone Ch6 Understanding Data Persistence/ContentView.swift
+++ In-Class Activity Pro IPhone Ch6 Understanding Data Persistence/ContentView.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
-
+class UserSettings: ObservableObject {
+    @Published var isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode") {
+        didSet {
+            UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
+        }
+    }
+}
 struct ContentView: View {
-    @State private var myToggle = false
+    @ObservedObject var userSettings = UserSettings()
     let coreDM: CoreDataManager
     @State var bookName = ""
     @State var bookAuthor = ""
@@ -17,55 +23,47 @@ struct ContentView: View {
     @State var bookArray = [Book]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.accent)
-            Text("Hello, world!")
-            Toggle(isOn: $myToggle, label: {
-                Text("Dark Mode")
+            Toggle("Dark Mode", isOn: $userSettings.isDarkMode)
+            .tint(.accentColor)
+            TextField("Enter book name", text: $bookName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter author", text: $bookAuthor)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter genre", text: $bookGenre)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter ISBN", text: $bookISBN)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save") {
+                coreDM.saveBook(name: bookName, genre: bookGenre, isbn: bookISBN, author: bookAuthor)
+                displayBooks()
+                bookName = ""
+                bookAuthor = ""
+                bookGenre = ""
+                bookISBN = ""
+            }
+            .padding()
+            List {
+                ForEach(bookArray, id: \.self) { book in
+                    VStack {
+                        Text(book.name ?? "")
+                        Text(book.author ?? "")
+                        Text(book.genre ?? "")
+                        Text(book.isbn ?? "")
+                    }
+                }.onDelete(perform: { indexSet in
+                    indexSet.forEach { index in
+                        let book = bookArray[index]
+                        coreDM.deleteBook(book: book)
+                        displayBooks()
+                    }
+                })
+            }
+            .onAppear(perform: {
+                displayBooks()
             })
         }
         .padding()
-        .onChange(of: myToggle) {
-            UserDefaults.standard.set(myToggle, forKey: "darkModeEnabled")
-            myToggle = UserDefaults.standard.bool(forKey: "darkModeEnabled")
-        }
-        .preferredColorScheme(UserDefaults.standard.bool(forKey: "darkModeEnabled") ? .light : .dark)
-        TextField("Enter book name", text: $bookName)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        TextField("Enter author", text: $bookAuthor)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        TextField("Enter genre", text: $bookGenre)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        TextField("Enter ISBN", text: $bookISBN)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        Button("Save") {
-            coreDM.saveBook(name: bookName, genre: bookGenre, isbn: bookISBN, author: bookAuthor)
-            displayBooks()
-            bookName = ""
-            bookAuthor = ""
-            bookGenre = ""
-            bookISBN = ""
-        }
-        List {
-            ForEach(bookArray, id: \.self) { book in
-                VStack {
-                    Text(book.name ?? "")
-                    Text(book.author ?? "")
-                    Text(book.genre ?? "")
-                    Text(book.isbn ?? "")
-                }
-            }.onDelete(perform: { indexSet in
-                indexSet.forEach { index in
-                    let book = bookArray[index]
-                    coreDM.deleteBook(book: book)
-                    displayBooks()
-                }
-            })
-        }
-        .onAppear(perform: {
-            displayBooks()
-        })
+        .preferredColorScheme(userSettings.isDarkMode ? .dark : .light)
     }
     func displayBooks() {
         bookArray = coreDM.getAllBooks()
